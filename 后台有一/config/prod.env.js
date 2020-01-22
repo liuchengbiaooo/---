8fc6457aaccaaa895f69db12a59f75605960281b
liuchengbiao <13058549341@163.com>
2020-01-22 13:29:44 +0800

    第一次提交

--- 后台有一/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  BASE_API: '"http://192.168.2.46:3000"'
-  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-}
--- 后台有一/src/js/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  url: 'http://192.168.2.46:3000',
-  mode: 'test'
-}
